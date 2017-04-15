70dd4b7dabd99c18b5593025c2725c014574ea68
Byron Blocker <byronblocker@gmail.com>
2017-04-14 21:05:03 -0700

    change fixnums to floating numbers

--- ex3.2.rb
+++ ex3.2.rb
@@ -1,4 +1,4 @@
-puts "Doggy day care will cost $30 three times a week: #{30 * 3}"
-puts "Dog walking will cost $15 twice a week: #{15 * 2}"
-puts "Together that will cost: #{(30 * 3) + (15 * 2)}"
-puts "For a 3 month program that will total #{((30 * 3) + (15 * 2)) * 3}"
+puts "Doggy day care will cost $30 three times a week: #{30.0 * 3.0}"
+puts "Dog walking will cost $15 twice a week: #{15.0 * 2.0}"
+puts "Together that will cost: #{(30.0 * 3.0) + (15.0 * 2.0)}"
+puts "For a 3 month program that will total #{((30.0 * 3.0) + (15.0 * 2.0)) * 3.0}"
