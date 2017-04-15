133dea139e3dbedee84cfe9d35ce3c37693e70b7
Byron Blocker <byronblocker@gmail.com>
2017-04-14 21:24:36 -0700

    practice using variables

---
+++ ex5.rb
@@ -0,0 +1,14 @@
+name = "Byron Blocker"
+age = 36
+height = 76 #inches
+weight = 220 #lbs
+eyes = "Hazel"
+teeth = "White"
+hair = "Black"
+
+puts "Lets talk about #{name}"
+puts "He is #{height} inches tall"
+puts "He weighs #{weight} pounds"
+puts "But trying to get it lower..."
+puts "He has #{eyes} eyes and #{hair} hair"
+puts "He tries to keep his teeth as #{teeth} as possible"
