dd68a8be992bbc4faaf5a495af475443483cb794
Byron Blocker <byronblocker@gmail.com>
2017-04-14 20:20:47 -0700

    create exercise file 2

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+#This is a comment so you can read your program later
+#Anything after the # is ignored
+
+puts 'I could have code like this.' #and the comment after is ignored
+
+#You can also use a comment "disable a piece of code:
+#puts 'this is code'
+
+puts 'this will run'
