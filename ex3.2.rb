fb25d89ac6e93daeb7785c9c86ca2ae216cd1d3b
Byron Blocker <byronblocker@gmail.com>
2017-04-14 21:02:02 -0700

    build program to figure out dog costs during school

---
+++ ex3.2.rb
@@ -0,0 +1,4 @@
+puts "Doggy day care will cost $30 three times a week: #{30 * 3}"
+puts "Dog walking will cost $15 twice a week: #{15 * 2}"
+puts "Together that will cost: #{(30 * 3) + (15 * 2)}"
+puts "For a 3 month program that will total #{((30 * 3) + (15 * 2)) * 3}"
